6b422d44157d21faa90ecc8797ee2cc93e65b8f3
aghogho <monorienaghogho@gmail.com>
2024-09-23 15:38:33 +0100

    Week01 - Course Home Page

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -1,21 +1,10 @@
-const courseList = [
-    "CSE 110",
-    "WDD 130",
-    "CSE 111",
-    "CSE 210",
-    "WDD 131",
-    "WDD 231",
-]
+
 
 
 document.addEventListener("DOMContentLoaded", () => {
-    const getYear = document.querySelector("#currentyear");
-    const lastModified = document.querySelector("#lastModified");
 
-    const today = new Date();
 
-    getYear.innerHTML = `${today.getFullYear()}`;
-    lastModified.innerHTML = `Last Update: ${today.getDate()}/${today.getMonth()}/${today.getFullYear()} ${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`
+
 
     const nav = document.querySelector("nav");
     const menu = document.querySelector("#menu");
@@ -23,13 +12,5 @@ document.addEventListener("DOMContentLoaded", () => {
     menu.addEventListener("click", () => {
         nav.classList.toggle("open");
         menu.classList.toggle("open");
-
-        nav.querySelectorAll('a').forEach(link => {
-            if (nav.classList.contains('open')) {
-                link.style.display = 'block';
-            } else {
-                link.style.display = 'none';
-            }
-        });
-    });
-});
+    })
+})
