166fbe8b71d8653a7a4fdc2133fd2a18aca985ee
aghogho <monorienaghogho@gmail.com>
2024-09-23 16:02:27 +0100

    Week01 - Added JS for Footer

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -3,14 +3,20 @@
 
 document.addEventListener("DOMContentLoaded", () => {
 
-
-
-
     const nav = document.querySelector("nav");
     const menu = document.querySelector("#menu");
 
     menu.addEventListener("click", () => {
         nav.classList.toggle("open");
         menu.classList.toggle("open");
-    })
+    });
+
+    const getYear = document.querySelector("#currentyear");
+    const lastModified = document.querySelector("#lastModified");
+
+    const today = new Date();
+
+    getYear.innerHTML = `${today.getFullYear()}`;
+    lastModified.innerHTML = `Last Update: ${today.getDate()}/${today.getMonth()}/${today.getFullYear()} ${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`
+
 })
