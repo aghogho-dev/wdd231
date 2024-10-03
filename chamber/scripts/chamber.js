3c2a48cd3b8974fc2e2f811a40ed0bd0baa6670f
aghogho <monorienaghogho@gmail.com>
2024-10-04 00:36:53 +0100

    PH CC - Mobile View almost done

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -5,3 +5,11 @@ menuBtn.addEventListener("click", () => {
     navigation.classList.toggle("open");
     menuBtn.classList.toggle("open");
 });
+
+const getYear = document.querySelector("#current-year");
+const lastModified = document.querySelector("#last-modified");
+
+const today = new Date();
+
+getYear.innerHTML = `${today.getFullYear()}`;
+lastModified.innerHTML = `Last Modification: ${today.getDate()}/${today.getMonth()}/${today.getFullYear()} ${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`
