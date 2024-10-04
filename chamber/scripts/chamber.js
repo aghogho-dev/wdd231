7decb73786d114b9e1d08b276dac4a6e3e4e8dc9
aghogho <monorienaghogho@gmail.com>
2024-10-04 04:19:48 +0100

    WIP

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -52,6 +52,11 @@ const displayData = (companies) => {
         companyIcon.setAttribute("width", "150");
         companyIcon.setAttribute("height", "150");
 
+        companyIcon.onerror = () => {
+            console.error(`Failed to load image: ${company.icon}`);
+            companyIcon.src = "https://placehold.co/50"
+        };
+
         email.innerHTML = `<strong>EMAIL:</strong> ${company.email}`;
         phone.innerHTML = `<strong>PHONE:</strong> ${company.phone}`;
         urlAddress.innerHTML = `<strong>URL:</strong> ${company.website}`;
